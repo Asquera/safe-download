0207cef47196f0ce802eaf7504be0f705b27c264
Felix Gilcher <felix.gilcher@asquera.de>
2013-05-28 11:55:51 +0000

    lightweight configuration mechanism
    make the secret, the ttl and the allowable strategies configurable. add
    a sample configuration file.

--- app.rb
+++ app.rb
@@ -1,17 +1,29 @@
 require 'sinatra/base'
 require 'hmac/strategies/query'
 require 'hmac/strategies/header'
+require 'yaml'
+require 'erb'
 
 class SafeDownloads < Sinatra::Base
   set :sessions, true # warden requires sessions, even if store => false is set
 
   use Warden::Manager do |manager|
     manager.failure_app = -> env { [401, {"Content-Length" => "0"}, [""]] }
-  # other scopes
-    manager.scope_defaults :default, :strategies => [:hmac_query, :hmac_header], 
+
+    config_file = "#{File.dirname(__FILE__)}/config/authentication.yml"
+
+    if File.exists? config_file
+      config = YAML.load(ERB.new(File.read(config_file)).result)[ENV['RACK_ENV']]
+      config[:ttl] ||= 300 # 5 minutes
+    else
+      raise "\nAuthentication config not found. Create a config file at: #{config_file}"
+    end
+
+    manager.scope_defaults :default, :strategies => config[:strategies], 
       :store => false, 
       :hmac => { 
-        :secret => "testsecret" 
+        :secret => config[:secret],
+        :ttl => config[:ttl]
       }
   end
 
